a139b1d52d518c15beee786606de2545fc08468a
Robin van Dijk <r.vandijk@developers.nl>
2019-05-15 16:05:44 +0200

    Add vue template loader for jest, so jest handles precompiled templates correctly

--- test/jest.imageshots.spec.js
+++ test/jest.imageshots.spec.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import initStoryshots, {multiSnapshotWithOptions} from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+
 const getMatchOptions = ({ context: { kind, story }, url }) => {
     return {
         failureThreshold: 0.1,
@@ -8,8 +9,16 @@ const getMatchOptions = ({ context: { kind, story }, url }) => {
     };
 };
 
+const beforeScreenshot = (page) => {
+    return new Promise(resolve =>
+        setTimeout(() => {
+            resolve();
+        }, 200)
+    );
+};
+
 initStoryshots({
     framework: 'vue',
     configPath: path.join(__dirname, '../.storybook'),
-    test: imageSnapshot({ storybookUrl: `file://${path.join(__dirname, '../storybook-static')}`, getMatchOptions })
+    test: imageSnapshot({ storybookUrl: `file://${path.join(__dirname, '../storybook-static')}`, getMatchOptions, beforeScreenshot })
 });
