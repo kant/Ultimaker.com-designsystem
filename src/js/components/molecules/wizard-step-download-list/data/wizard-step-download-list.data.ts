82fcb0f83265bee248eac16d43aeb8d3481ed28b
Matijs Brinkhuis <matijs@gmail.com>
2019-06-13 10:51:40 +0200

    chore: fix linting

--- src/js/components/molecules/wizard-step-download-list/data/wizard-step-download-list.data.ts
+++ src/js/components/molecules/wizard-step-download-list/data/wizard-step-download-list.data.ts
@@ -1,3 +1,4 @@
+/** @format */
 import image from 'components/atoms/c-image/data/examples/c-image-nylon';
 
 export const data = {
@@ -5,29 +6,34 @@ export const data = {
         type: 'WizardStepDownloadList',
         title: 'Download Cura 4.0 now!',
         subtitle: 'Subtle subtitle',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan ut orci ac commodo. Sed rhoncus, urna ut venenatis cursus, nunc turpis suscipit enim, in congue dui dui quis sem.',
-        items: [{
-            image,
-            description: 'Windows, 64 bit',
-            file: 'http://speed.transip.nl/1gb.bin?win',
-            title: 'Ultimaker Cura 4.0',
-            name: 'download',
-            selected: true,
-        }, {
-            image,
-            description: 'Mac, 64 bit',
-            file: 'http://speed.transip.nl/1gb.bin?mac',
-            title: 'Ultimaker Cura 4.0',
-            name: 'download',
-            selected: false,
-        }, {
-            image,
-            description: 'Linux, 64 bit',
-            file: 'http://speed.transip.nl/1gb.bin?linux',
-            title: 'Ultimaker Cura 4.0',
-            name: 'download',
-            selected: false,
-        }],
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan ut orci ac commodo. Sed rhoncus, urna ut venenatis cursus, nunc turpis suscipit enim, in congue dui dui quis sem.',
+        items: [
+            {
+                image,
+                description: 'Windows, 64 bit',
+                file: 'http://speed.transip.nl/1gb.bin?win',
+                title: 'Ultimaker Cura 4.0',
+                name: 'download',
+                selected: true,
+            },
+            {
+                image,
+                description: 'Mac, 64 bit',
+                file: 'http://speed.transip.nl/1gb.bin?mac',
+                title: 'Ultimaker Cura 4.0',
+                name: 'download',
+                selected: false,
+            },
+            {
+                image,
+                description: 'Linux, 64 bit',
+                file: 'http://speed.transip.nl/1gb.bin?linux',
+                title: 'Ultimaker Cura 4.0',
+                name: 'download',
+                selected: false,
+            },
+        ],
         submitButton: {
             label: 'Download now',
         },
@@ -35,24 +41,28 @@ export const data = {
     'two-options': {
         type: 'WizardStepDownloadList',
         title: 'Download Firmware',
-        description: 'Make sure to download the correct firmware for your printer.',
-        items: [{
-            image,
-            description: 'Ultimaker 2+',
-            file: 'http://speed.transip.nl/1gb.bin?win',
-            title: 'Ultimaker 2+ Firmware v1.0',
-            name: 'download',
-            selected: false,
-        }, {
-            image,
-            description: 'Ultimaker S5',
-            file: 'http://speed.transip.nl/1gb.bin?mac',
-            title: 'Ultimaker S5 Firmware v1.0',
-            name: 'download',
-            selected: false,
-        }],
+        description:
+            'Make sure to download the correct firmware for your printer.',
+        items: [
+            {
+                image,
+                description: 'Ultimaker 2+',
+                file: 'http://speed.transip.nl/1gb.bin?win',
+                title: 'Ultimaker 2+ Firmware v1.0',
+                name: 'download',
+                selected: false,
+            },
+            {
+                image,
+                description: 'Ultimaker S5',
+                file: 'http://speed.transip.nl/1gb.bin?mac',
+                title: 'Ultimaker S5 Firmware v1.0',
+                name: 'download',
+                selected: false,
+            },
+        ],
         submitButton: {
-            label: 'Download'
+            label: 'Download',
         },
     },
 };
